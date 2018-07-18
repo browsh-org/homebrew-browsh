bac3319b44774497a043fc636b03bdeb3a8771c6
Goreleaser Bot care of Travis CI <builds@travis-ci.com>
2018-07-18 19:51:22 +0700

    Brew formula update for browsh version v1.4.0

--- browsh.rb
+++ browsh.rb
@@ -1,11 +1,16 @@
 class Browsh < Formula
   desc "The modern, text-based browser"
   homepage "https://www.brow.sh"
-  url "https://github.com/browsh-org/browsh/releases/download/v1.3.3/browsh_1.3.3_darwin_amd64.tar.gz"
-  version "1.3.3"
-  sha256 "fa0e4637baf1659845f6ae1bc436e3dc7182c6321020d082e501a4248f6c9651"
+  url "https://github.com/browsh-org/browsh/releases/download/v1.4.0/browsh_1.4.0_darwin_amd64.tar.gz"
+  version "1.4.0"
+  sha256 "937942c7c39e1e4fe7703d671b06281a100506efffc9cf900722fb655dbb74eb"
 
   def install
     bin.install "browsh"
+  end
+
+  def caveats; <<~EOS
+    You need Firefox 57 or newer to run Browsh
+  EOS
   end
 end
