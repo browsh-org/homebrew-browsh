be932ba3cc6d42c43271d937e9a3cb2020a5c48f
Goreleaser Bot care of Travis CI <builds@travis-ci.com>
2018-08-02 18:34:30 +0700

    Brew formula update for browsh version v1.4.12

--- browsh.rb
+++ browsh.rb
@@ -1,9 +1,9 @@
 class Browsh < Formula
   desc "The modern, text-based browser"
   homepage "https://www.brow.sh"
-  url "https://github.com/browsh-org/browsh/releases/download/v1.4.11/browsh_1.4.11_darwin_amd64.tar.gz"
-  version "1.4.11"
-  sha256 "3c19373d561f5a984b5b912c0d8ff26b8d08573b4d900370d851d82ddeb6b7e3"
+  url "https://github.com/browsh-org/browsh/releases/download/v1.4.12/browsh_1.4.12_darwin_amd64.tar.gz"
+  version "1.4.12"
+  sha256 "ea37ed94cfe84f7e9af16513a077a2d30bfb59e38b3b26aa2b68bab2daeae973"
 
   def install
     bin.install "browsh"
